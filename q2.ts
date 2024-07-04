711e2e606ec5882fbd307b3dee79178e414ebba3
Apicha660612162 <apichalerdjintanakid@gmail.com>
2024-07-04 17:42:53 +0700

    gg

--- q2.ts
+++ q2.ts
@@ -1,13 +1,23 @@
+//660612162 อภิชา เลฺิศจินตนกิจ
 // define interface for Student object
+interface Student {
+  name:string;
+  score:number;
+}
 /* Your code here */
+  
 
 // assign interface/type to the function definition properly
-function findTopNames(students) {
+function findTopNames(students:Student[]):string[] {
   /* Your code here */
-}
+ 
+return students
+.filter((students)=> students.score > 8)
+.map((students) => students.name)
+}   
 
 // assign interface/type to the student1 object properly
-const students1 = [
+const students1: Student[]= [
   { name: "john", score: 10 },
   { name: "jane", score: 9 },
   { name: "jim", score: 8 },
